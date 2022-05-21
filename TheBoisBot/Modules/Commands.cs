04ca0169f9e19db729ac630e0c59c28ebc29c592
HUNTERSCOUTI <39131351+HUNTERSCOUTI@users.noreply.github.com>
2022-05-21 23:45:43 +0200

    Setup(Ping Pong)

---
+++ TheBoisBot/Modules/Commands.cs
@@ -0,0 +1,14 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+
+namespace TheBoisBot.Modules;
+
+public class Commands : ModuleBase<SocketCommandContext>
+{
+    [SlashCommand("Ping", "Play a bit of ping pong")]
+    public async Task Ping()
+    {
+        await ReplyAsync("Pong");
+    }
+}
---
+++ TheBoisBot/Modules/InteractionModule.cs
@@ -0,0 +1,46 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+
+namespace TheBoisBot.Menus;
+
+public class InteractionModule : InteractionModuleBase<SocketInteractionContext>
+{
+    [SlashCommand("menu", "Demonstrate menu")]
+    public async Task HandleMenuCommand()
+    {
+        var menu = new SelectMenuBuilder()
+        {
+            CustomId = "menu",
+            Placeholder = "Sample Menu"
+        };
+        menu.AddOption("Option A", "opt-a", "Option B is lying!");
+        menu.AddOption("Option B", "opt-b", "Option A is telling the truth!");
+
+        var component = new ComponentBuilder();
+        component.WithSelectMenu(menu);
+
+        await RespondAsync("testing", components: component.Build());
+    }
+
+    [ComponentInteraction("menu")]
+    public async Task HandleMenuSelection(string[] inputs)
+    {
+        await RespondAsync(inputs[0]);
+    }
+
+    [ModalInteraction("demo_modal1")]
+    public async Task HandleModalInput(DemoModal modal)
+    {
+        string input = modal.Greeting;
+        await RespondAsync(input);
+    }
+}
+
+public class DemoModal : IModal
+{
+    public string Title => "Demo Model";
+    [InputLabel("Send a greeting!")]
+    [ModalTextInput("greeting_input", TextInputStyle.Short, placeholder: "Be nice...", maxLength: 100)]
+    public string Greeting { get; set; }
+}
